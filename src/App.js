eec98e67b25123a9b44b0999498dcb94f769a568
Poojalaksh2002 <lakshmipooja232@gmail.com>
2023-06-21 19:18:32 +0530

    Media_query_done_for_mainPg

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBackward } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="d-flex flex-column align-items-center justify-content-between vh-100 ">
-      <div className="fs-1 fs-xs-1 fs-sm-2 fs-md-5">
-        <h1 className="text-center">Calling on the Name of the Lord</h1>
-        <p className="text-center">
+    <div className="d-flex flex-column align-items-center justify-content-between vh-100">
+      <div className="container-text p-4">
+        <h1 className="heading text-center">Calling on the Name of the Lord</h1>
+        <p className="paragraph text-center mt-4">
           Calling on the Name of the Lord to set our mind on the spirit-1
           Cor.12:3;Rom.8:6; 10:12-13
         </p>
       </div>
 
       <div>
-        <h4>5</h4>
+        <h6 className="counter">5</h6>
       </div>
 
-      <div className="d-flex justify-content-around vw-100">
-        <button className="btn btn-dark bg-black btn-hover-blue">
+      <div className="d-flex justify-content-between vw-100">
+        <button className="btn btn-dark bg-black btn-hover-blue btn-back ">
           <FontAwesomeIcon icon={faBackward} />
         </button>
-        <button className="btn btn-dark bg-black btn-hover-blue">
+        <button className="btn btn-dark bg-black btn-hover-blue btn-continue ">
           Continue
         </button>
-        <button className="exit btn btn-danger">Exit</button>
+        <button className="exit btn btn btn-danger ">Exit</button>
       </div>
     </div>
   );
